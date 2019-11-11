7a19e5efbd889c1fa139412417f368af859ef297
glitchub <glitchub@users.noreply.github.com>
2019-11-11 10:14:37 -0800

    wip

--- drivers/sr04.c
+++ drivers/sr04.c
@@ -1,19 +1,22 @@
 // SR04 ultrasonic ranging driver
 
+// 8-cycle loops in N microseconds, for MHZ == 8 or 16
+#define uS(N) (N*(MHZ/8U))
+
 // The SR04 module has TRIG input and ECHO output, normally both low. When the
 // module sees TRIG go high for 10uS it transmits a burst of ultrasonic pulses
 // and sets ECHO high. ECHO goes low when echo of the burst is received, or on
 // timeout.
 
-// 8-cycle loops in N microseconds, assumes MHZ is 8 or 16
-#define uS(N) (N*(MHZ/8U))
+// get_sr04() can spin with interrupts disabled for up to 26 mS
+#if !defined(TICKMS) || TICKMS < 16
+#warning TICKMS should be at least 16 to avoid losing ticks
+#endif
 
-#define WRONG
-
-// Trigger the SR04 and return echo pulse width converted to centimeters.
-// Return -2 if SR04 didn't respond,l or -1 if no echo was received within 25
-// mS.  This will block with interrupts disabled up to 26 mS.  Do not call more
-// often than once per 100mS.
+// Given trigger and echo gpios, trigger the SR04 and return the echo pulse
+// width converted to centimeters. Return -2 if SR04 didn't respond, or -1 if
+// no echo was received within 25 mS.
+// Do not call more often than once per 100 mS.
 int16_t get_sr04(gpio *trigger, gpio *echo)
 {
     out_gpio(trigger);
@@ -30,45 +33,37 @@ int16_t get_sr04(gpio *trigger, gpio *echo)
 
     // wait 1mS for echo to go high
     uint16_t loops=uS(1000);
-#ifdef WRONG
-    volatile uint8_t *pin = echo->pin;
-    uint8_t bit = echo->bit;
-    while (--loops) if (*pin & bit) break;
-#else
-    asm("1:                     \n"
-        "   nop                 \n" // 1 cycle
-        "   sbiw X, 1           \n" // 2 cycles, decrement loops
-        "   breq 2f             \n" // 1 cycle, exit if zero
-        "   ld r0, Z            \n" // 1 cycle, get port
-        "   and r0, %1          \n" // 1 cycle, test bit
-        "   breq 1b             \n" // 2 cycles, repeat if clear
-        "2:                     \n"
-        : "=x" (loops)
-        : "0" (loops), "r" (echo->bit), "z" (echo->pin)
+    asm volatile (
+        // loop is 8 cycles
+        "; loops=%0 (%a0) bit=%2 *pin=%3 (%a3)\n"
+        "1: sbiw %a0,  \n" // 2, decrement loops
+        "   breq 2f    \n" // 1, exit if zero
+        "   ld r0, %a3 \n" // 2, get *pin
+        "   and r0, %2 \n" // 1, test bit
+        "   breq 1b    \n" // 2, repeat if clear
+        "2:            \n"
+        : "=&e" (loops)
+        : "0" (loops), "r" (echo->bit), "e" (echo->pin)
     );
-#endif
     if (!loops)
     {
         SREG = sreg;
         return -2;
     }
-    // wait 25 mS for echo to go low
+    // wait 25 mS for echo to go low, loop is 8 cycles
     loops=uS(25000);
-#ifdef WRONG
-    while (--loops) if (!(*pin & bit)) break;
-#else
-    asm("1:                     \n"
-        "    nop                \n" // 1 cycle
-        "    sbiw X, 1          \n" // 2 cycles, decrement loops
-        "    breq 2f            \n" // 1 cycle,  done if loops == 0
-        "    ld r0, Z           \n" // 1 cycle,  get echo->port
-        "    and r0, %1         \n" // 1 cycle,  test bit
-        "    brne 1b            \n" // 2 cycles, repeat if set
-        "2:                     \n"
-        : "=x" (loops)
-        : "0" (loops), "r" (echo->bit), "z" (echo->pin)
+    asm volatile (
+        // loop is 8 cycles
+        "; loops=%0 (%a0) bit=%2 *pin=%3 (%a3)\n"
+        "1: sbiw %a0, 1\n" // 2, decrement loops
+        "   breq 2f    \n" // 1, exit if zero
+        "   ld r0, %a3 \n" // 2, get *pin
+        "   and r0, %2 \n" // 1, test bit
+        "   brne 1b    \n" // 2, repeat if set
+        "2:            \n"
+        : "=&e" (loops)
+        : "0" (loops), "r" (echo->bit), "e" (echo->pin)
     );
-#endif
     SREG = sreg;
     if (!loops) return -1;
 
--- sr04demo/main.h
+++ sr04demo/main.h
@@ -1,2 +1,2 @@
 #define BOARD "uno_r3.h"
-#define TICKMS 16           // sr04 driver can turn off interrupts up to 26mS
+#define TICKMS 16 // sr04 driver can turn off interrupts up to 26mS
